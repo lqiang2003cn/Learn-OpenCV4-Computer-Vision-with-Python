7cebb11e087ff6e0047f3a5d868e31f34721ebed
lqiang2003cn <randycoq@163.com>
2024-06-01 11:28:10 +0800

    d

--- chapter3/cameo/cameo.py
+++ chapter3/cameo/cameo.py
@@ -2,13 +2,14 @@ import cv2
 import filters
 from managers import WindowManager, CaptureManager
 
+
 class Cameo(object):
 
     def __init__(self):
         self._windowManager = WindowManager('Cameo',
                                             self.onKeypress)
         self._captureManager = CaptureManager(
-            cv2.VideoCapture(0), self._windowManager, True)
+            cv2.VideoCapture("../../chapter8/pedestrians.avi"), self._windowManager, True)
         self._curveFilter = filters.BGRPortraCurveFilter()
 
     def run(self):
@@ -33,16 +34,17 @@ class Cameo(object):
         escape -> Quit.
 
         """
-        if keycode == 32: # space
+        if keycode == 32:  # space
             self._captureManager.writeImage('screenshot.png')
-        elif keycode == 9: # tab
+        elif keycode == 9:  # tab
             if not self._captureManager.isWritingVideo:
                 self._captureManager.startWritingVideo(
                     'screencast.avi')
             else:
                 self._captureManager.stopWritingVideo()
-        elif keycode == 27: # escape
+        elif keycode == 27:  # escape
             self._windowManager.destroyWindow()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     Cameo().run()
--- chapter3/canny.py
+++ chapter3/canny.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("statue.jpg", 0)
-cv2.imwrite("canny.jpg", cv2.Canny(img, 200, 300))
+img = cv2.imread("statue_small.jpg", 0)
+# Width and Height
+cv2.imwrite("canny.jpg", cv2.Canny(img, img.shape[1], img.shape[0]))
 cv2.imshow("canny", cv2.imread("canny.jpg"))
 cv2.waitKey()
 cv2.destroyAllWindows()
